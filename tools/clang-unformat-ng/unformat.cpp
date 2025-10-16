0a3dd2ec3dced40f358371c618f4f67a54660366
Jevin Sweval <jevinsweval@gmail.com>
2025-10-16 14:25:56 -0400

    cleanup

--- tools/clang-unformat-ng/unformat.cpp
+++ tools/clang-unformat-ng/unformat.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <map>
 #include <regex>
 #include <set>
@@ -42,264 +43,23 @@ template <> struct fmt::formatter<clang::tooling::Replacement> {
     }
     constexpr auto format(const clang::tooling::Replacement &r, fmt::format_context &ctx) const
         -> fmt::format_context::iterator {
-        fmt::format_to(ctx.out(), "Replacement {{ ");
+        fmt::format_to(ctx.out(), "R1 {{ ");
         fmt::format_to(ctx.out(), "{}", r.toString());
         fmt::format_to(ctx.out(), " }}");
         return ctx.out();
     }
 };
 
-#if 0
+#if 1
 template <> struct fmt::formatter<clang::tooling::Replacements> : fmt::formatter<fmt::string_view> {
     constexpr auto format(const clang::tooling::Replacements &rs, fmt::format_context &ctx) const
         -> fmt::format_context::iterator {
-        fmt::format_to(ctx.out(), "Replacements {{ ");
+        fmt::format_to(ctx.out(), "Rn {{ ");
         fmt::format_to(ctx.out(), "{}", fmt::join(rs, " "));
         fmt::format_to(ctx.out(), " }}");
         return ctx.out();
     }
 };
-#endif
-
-#if 0
-// https://github.com/shao-hua-li/UBGen
-
-namespace ruleactioncallback {
-
-class RuleActionCallback : public clang::ast_matchers::MatchFinder::MatchCallback {
-public:
-    RuleActionCallback(clang::transformer::RewriteRuleWith<void> Rule,
-                       std::map<std::string, clang::tooling::Replacements> &FileToReplacements,
-                       std::map<std::string, int> &FileToNumberValueTrackers);
-    void run(const clang::ast_matchers::MatchFinder::MatchResult &Result) override;
-    void registerMatchers(clang::ast_matchers::MatchFinder &Finder);
-    std::string getFunctionAsText(const clang::Decl *F, const clang::SourceManager &SM, const clang::LangOptions &lp);
-
-private:
-    clang::transformer::RewriteRuleWith<void> Rule;
-    std::map<std::string, clang::tooling::Replacements> &FileToReplacements;
-    std::map<std::string, int> &FileToNumberValueTrackers;
-};
-
-std::string GetFilenameFromRange(const CharSourceRange &R, const SourceManager &SM);
-RangeSelector startOfFile(std::string ID);
-
-} // namespace ruleactioncallback
-
-std::string ruleactioncallback::GetFilenameFromRange(const CharSourceRange &R, const SourceManager &SM) {
-    const std::pair<FileID, unsigned> DecomposedLocation = SM.getDecomposedLoc(SM.getSpellingLoc(R.getBegin()));
-    const FileEntry *Entry                               = SM.getFileEntryForID(DecomposedLocation.first);
-    return std::string(Entry ? Entry->tryGetRealPathName() : "");
-}
-
-Expected<DynTypedNode> getNode(const ast_matchers::BoundNodes &Nodes, StringRef ID) {
-    auto &NodesMap = Nodes.getMap();
-    auto It        = NodesMap.find(ID);
-    if (It == NodesMap.end())
-        return llvm::make_error<llvm::StringError>(llvm::errc::invalid_argument, ID + "not bound");
-    return It->second;
-}
-
-RangeSelector startOfFile(std::string ID) {
-    return [ID](const clang::ast_matchers::MatchFinder::MatchResult &Result) -> Expected<CharSourceRange> {
-        auto Node = getNode(Result.Nodes, ID);
-        if (!Node)
-            return Node.takeError();
-        const auto &SM = Result.Context->getSourceManager();
-        auto Start     = SM.getLocForStartOfFile(SM.getFileID(Node->getSourceRange().getBegin()));
-        return CharSourceRange(SourceRange(Start), false);
-    };
-}
-
-ruleactioncallback::RuleActionCallback::RuleActionCallback(
-    RewriteRuleWith<void> Rule, std::map<std::string, clang::tooling::Replacements> &FileToReplacements,
-    std::map<std::string, int> &FileToNumberValueTrackers)
-    : Rule{Rule}, FileToReplacements{FileToReplacements}, FileToNumberValueTrackers{FileToNumberValueTrackers} {}
-
-void ruleactioncallback::RuleActionCallback::run(const clang::ast_matchers::MatchFinder::MatchResult &Result) {
-
-    if (Result.Context->getDiagnostics().hasErrorOccurred()) {
-        llvm::errs() << "An error has occured.\n";
-        return;
-    }
-    // Expected<SmallVector<transformer::Edit, 1>> Edits =
-    //     transformer::detail::findSelectedCase(Result, Rule).Edits(Result);
-    // if (!Edits) {
-    //     llvm::errs() << "Rewrite failed: " << llvm::toString(Edits.takeError()) << "\n";
-    //     return;
-    // }
-    const auto sel_case = transformer::detail::findSelectedCase(Result, Rule);
-    // if (1 != transformer::detail::findSelectedCase(Result, Rule)) {
-    //     llvm::errs() << "Rewrite failed: " << Rule.Metadata.begin()->get()->toString() << "\n";
-    //     return;
-    // }
-    auto SM        = Result.SourceManager;
-    const auto &EG = Rule.Cases[sel_case].Edits;
-    // const auto &EGV = EG(Result);
-    // const auto &EGV = flatten(EG);
-    Expected<SmallVector<transformer::Edit, 1>> Edits = EG(Result);
-    if (!Edits) {
-        llvm::errs() << "Rewrite failed: " << llvm::toString(Edits.takeError()) << "\n";
-        return;
-    }
-    for (const auto &T : *Edits) {
-        auto FilePath      = GetFilenameFromRange(T.Range, *SM);
-        auto N             = FileToNumberValueTrackers[FilePath]++;
-        auto R             = tooling::Replacement(*SM, T.Range, T.Replacement);
-        auto &Replacements = FileToReplacements[FilePath];
-        auto Err           = Replacements.add(R);
-        if (Err) {
-            auto NewOffset = Replacements.getShiftedCodePosition(R.getOffset());
-            auto NewLength = Replacements.getShiftedCodePosition(R.getOffset() + R.getLength()) - NewOffset;
-            if (NewLength == R.getLength()) {
-                R = clang::tooling::Replacement(R.getFilePath(), NewOffset, NewLength, R.getReplacementText());
-                Replacements = Replacements.merge(tooling::Replacements(R));
-            } else {
-                llvm_unreachable(llvm::toString(std::move(Err)).c_str());
-            }
-        }
-    }
-}
-
-void ruleactioncallback::RuleActionCallback::registerMatchers(clang::ast_matchers::MatchFinder &Finder) {
-    for (auto &Matcher : transformer::detail::buildMatchers(Rule))
-        Finder.addDynamicMatcher(Matcher.withTraversalKind(clang::TK_IgnoreUnlessSpelledInSource), this);
-}
-
-class AddBraces {
-public:
-    AddBraces(std::map<std::string, clang::tooling::Replacements> &FileToReplacements);
-    AddBraces(const AddBraces &) = delete;
-    AddBraces(AddBraces &&)      = delete;
-
-    void registerMatchers(clang::ast_matchers::MatchFinder &Finder);
-
-private:
-    std::map<std::string, clang::tooling::Replacements> &FileToReplacements;
-    std::vector<ruleactioncallback::RuleActionCallback> Callbacks;
-    std::map<std::string, int> FileToNumberValueTrackers;
-};
-
-namespace {
-
-/* Self-defined computations/actions */
-
-// add braces around if/while/for/switch nodes
-auto curlyBraceAction = {insertBefore(statement("stmt"), cat("{\n")), insertAfter(statement("stmt"), cat("\n}"))};
-
-/* Matchers and Rules */
-
-AST_MATCHER_P(CaseStmt, isCaseSubStmt, ast_matchers::internal::Matcher<Stmt>, InnerMatcher) {
-    const auto *SubStmt = Node.getSubStmt();
-    return (SubStmt != nullptr && InnerMatcher.matches(*SubStmt, Finder, Builder));
-}
-
-AST_MATCHER_P(DefaultStmt, isDefaultSubStmt, ast_matchers::internal::Matcher<Stmt>, InnerMatcher) {
-    const auto *SubStmt = Node.getSubStmt();
-    return (SubStmt != nullptr && InnerMatcher.matches(*SubStmt, Finder, Builder));
-}
-
-// rule for if/then statements without braces
-auto canonicalizeIfThenRule() {
-    return makeRule(ifStmt(isExpansionInMainFile(), hasThen(stmt(unless(compoundStmt())).bind("stmt"))),
-                    curlyBraceAction);
-}
-// rule for if/else statements without braces
-auto canonicalizeIfElseRule() {
-    return makeRule(ifStmt(isExpansionInMainFile(), hasElse(stmt(unless(compoundStmt())).bind("stmt"))),
-                    curlyBraceAction);
-}
-// rule for loop statements without braces
-auto canonicalizeLoopRule() {
-    return makeRule(mapAnyOf(forStmt, whileStmt, doStmt, cxxForRangeStmt)
-                        .with(isExpansionInMainFile(), hasBody(stmt(unless(compoundStmt())).bind("stmt"))),
-                    curlyBraceAction);
-}
-// rule for switch statements without braces
-auto canonicalizeSwitchRule() {
-    auto Unless = unless(anyOf(compoundStmt(), caseStmt(), defaultStmt()));
-    return applyFirst(
-        {makeRule(caseStmt(isExpansionInMainFile(), isCaseSubStmt(stmt(Unless).bind("stmt"))), curlyBraceAction),
-         makeRule(defaultStmt(isExpansionInMainFile(), isDefaultSubStmt(stmt(Unless).bind("stmt"))),
-                  curlyBraceAction)});
-}
-
-} // namespace
-
-AddBraces::AddBraces(std::map<std::string, clang::tooling::Replacements> &FileToReplacements)
-    : FileToReplacements{FileToReplacements} {
-
-    // canonicalize rules
-    Callbacks.emplace_back(ruleactioncallback::RuleActionCallback{canonicalizeIfThenRule(), FileToReplacements,
-                                                                  FileToNumberValueTrackers});
-    Callbacks.emplace_back(ruleactioncallback::RuleActionCallback{canonicalizeIfElseRule(), FileToReplacements,
-                                                                  FileToNumberValueTrackers});
-    Callbacks.emplace_back(
-        ruleactioncallback::RuleActionCallback{canonicalizeLoopRule(), FileToReplacements, FileToNumberValueTrackers});
-    Callbacks.emplace_back(ruleactioncallback::RuleActionCallback{canonicalizeSwitchRule(), FileToReplacements,
-                                                                  FileToNumberValueTrackers});
-}
-
-void AddBraces::registerMatchers(clang::ast_matchers::MatchFinder &Finder) {
-    for (auto &Callback : Callbacks)
-        Callback.registerMatchers(Finder);
-}
-
-using namespace llvm;
-
-namespace {
-
-cl::OptionCategory UnformatOptionsCategory("unformat");
-
-bool applyReplacements(clang::tooling::RefactoringTool &Tool) {
-    LangOptions DefaultLangOptions;
-    DiagnosticOptions DiagOpts;
-    clang::TextDiagnosticPrinter DiagnosticPrinter(errs(), DiagOpts);
-    DiagnosticsEngine Diagnostics(DiagnosticIDs::create(), DiagOpts, &DiagnosticPrinter, false);
-    auto &FileMgr = Tool.getFiles();
-    SourceManager Sources(Diagnostics, FileMgr);
-
-    Rewriter Rewrite(Sources, DefaultLangOptions);
-
-    bool Result = true;
-    for (const auto &FileAndReplaces :
-         groupReplacementsByFile(Rewrite.getSourceMgr().getFileManager(), Tool.getReplacements())) {
-        auto &CurReplaces = FileAndReplaces.second;
-
-        Result = applyAllReplacements(CurReplaces, Rewrite) && Result;
-    }
-    if (!Result) {
-        llvm::errs() << "Failed applying all replacements.\n";
-        return false;
-    }
-
-    return !Rewrite.overwriteChangedFiles();
-}
-
-template <typename InstrTool> int runToolOnCode(clang::tooling::RefactoringTool &Tool) {
-    auto &replacements = Tool.getReplacements();
-    fmt::print("init replacements: {}\n", replacements);
-    InstrTool Instr(replacements);
-    ast_matchers::MatchFinder Finder;
-    Instr.registerMatchers(Finder);
-    std::unique_ptr<tooling::FrontendActionFactory> Factory = tooling::newFrontendActionFactory(&Finder);
-
-    auto Ret = Tool.run(Factory.get());
-    if (!Ret) {
-        fmt::print("mid replacements: {}\n", Tool.getReplacements());
-        if (!applyReplacements(Tool)) {
-            fmt::print("error replacements: {}\n", Tool.getReplacements());
-            llvm::errs() << "Failed to overwrite the input files.\n";
-            return 1;
-        }
-    }
-
-    fmt::print("good replacements: {}\n", Tool.getReplacements());
-
-    return Ret;
-}
-
-} // namespace
 #endif
 
 #if 0
@@ -439,42 +199,30 @@ static bool format(StringRef FileName) {
 
 namespace unformat {
 namespace priv {
+
 static cl::OptionCategory UnformatOptionsCategory("unformat");
-static cl::list<std::string> FileNames(cl::Positional, cl::desc("[@<file>] [<file> ...]"),
-                                       cl::cat(UnformatOptionsCategory));
+static cl::list<std::string> FileNames(cl::Positional, cl::desc("[<file> ...]"), cl::cat(UnformatOptionsCategory));
+
+void construct_memfs(const std::vector<std::string> fnames) {}
+
+int priv_main() {
+    std::vector<std::string> fnames(FileNames.begin(), FileNames.end());
+    for (const auto &f : fnames) {
+        fmt::print("file: {}\n", f);
+    }
+    fmt::print(stderr, "fmt print stderr alice\n");
+    llvm::errs() << "llvm::errs() bob\n";
+
+    return 0;
+}
+
 }; // namespace priv
 }; // namespace unformat
 
 int main(int argc, const char **argv) {
     InitLLVM X(argc, argv);
-
     cl::HideUnrelatedOptions(unformat::priv::UnformatOptionsCategory);
     cl::ParseCommandLineOptions(argc, argv, "A tool to calculate \"optimal\" format styles from existing source.\n\n");
-
-    for (const auto FName : unformat::priv::FileNames) {
-        fmt::print("Fname: {}\n", FName);
-    }
-
-#if 0
-    auto ExpectedParser = clang::tooling::CommonOptionsParser::create(argc, argv, UnformatOptionsCategory);
-    if (!ExpectedParser) {
-        llvm::errs() << ExpectedParser.takeError();
-        return 1;
-    }
-    clang::tooling::CommonOptionsParser &OptionsParser = ExpectedParser.get();
-
-    const auto &Compilations = OptionsParser.getCompilations();
-    const auto &Files        = OptionsParser.getSourcePathList();
-    fmt::print("Files: {}\n", fmt::join(Files, " "));
-    clang::tooling::RefactoringTool Tool(Compilations, Files);
-    int Result = 0;
-    Result     = runToolOnCode<AddBraces>(Tool);
-
-    if (Result) {
-        llvm::errs() << "Something went wrong...\n";
-        return Result;
-    }
-#endif
-
-    return 0;
+    int res = unformat::priv::priv_main();
+    return res;
 }
