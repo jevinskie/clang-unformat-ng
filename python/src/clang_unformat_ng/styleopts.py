3d4fabbae9d971770d0c743e2dd9198c07c87608
Jevin Sweval <jevinsweval@gmail.com>
2025-10-18 19:13:24 -0400

    python wip

--- python/src/clang_unformat_ng/styleopts.py
+++ python/src/clang_unformat_ng/styleopts.py
@@ -1,3 +1,5 @@
 from clang_unformat_ng import styleopts_data
 
+# from clang_unformat_ng.styleopts_types import Type, Version, NestedField, EnumValue, Enum, NestedEnum, NestedStruct, Option
+
 opts = styleopts_data.opts
--- python/src/clang_unformat_ng/tools/unformat_util.py
+++ python/src/clang_unformat_ng/tools/unformat_util.py
@@ -2,14 +2,25 @@
 
 import argparse
 
+from rich.pretty import pprint
+
+import clang_unformat_ng.styleopts as sopts
+
+
+def dump_opts():
+    pprint(sopts.opts)
+
 
 def get_arg_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(prog="clang-unformat-ng-py-util")
+    parser.add_argument("--dump-opts", action="store_true", help="Dump style opts")
     return parser
 
 
 def real_main(args: argparse.Namespace):
     print(f"args: {args}")
+    if args.dump_opts:
+        dump_opts()
 
 
 def main():
