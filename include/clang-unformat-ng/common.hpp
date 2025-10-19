7c9eec9af79be64e99c2713351d376d995538339
Jevin Sweval <jevinsweval@gmail.com>
2025-10-19 02:26:40 -0400

    wip

--- include/clang-unformat-ng/common.hpp
+++ include/clang-unformat-ng/common.hpp
@@ -1,1 +1,9 @@
 #pragma once
+
+#if 0
+#include <string>
+using string_t = std::string;
+#else
+#include <stringzilla/stringzilla.hpp>
+using string_t = ashvardanian::stringzilla::string;
+#endif
--- python/src/clang_unformat_ng/styleopts_template.py
+++ python/src/clang_unformat_ng/styleopts_template.py
@@ -3,11 +3,16 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 env = Environment(
     loader=PackageLoader("clang_unformat_ng.styleopts"),
     autoescape=select_autoescape(
-        enabled_extensions=("cpp"),
+        enabled_extensions=("tcpp"),
         default_for_string=True,
     ),
 )
 
-template = env.get_template("bool.cpp")
+template = env.get_template("bool.tcpp")
 
 print(f"template: {template}")
+
+rnd = template.render()
+
+print("rnd:")
+print(rnd)
