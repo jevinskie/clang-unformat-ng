8f4d097d7cc5c9fa3d1e64784e06ae522d56aaa0
Jevin Sweval <jevinsweval@gmail.com>
2025-10-18 23:26:11 -0400

    python ffi wip

--- lib/clang-unformat-ng/python/cxx_py_ffi.cpp
+++ lib/clang-unformat-ng/python/cxx_py_ffi.cpp
@@ -1,9 +1,13 @@
 #include <nanobind/nanobind.h>
 
+namespace nb = nanobind;
+
+using namespace nb::literals;
+
 int add(int a, int b) {
     return a + b;
 }
 
-NB_MODULE(clang_unformat_ng_py_ffi, m) {
+NB_MODULE(cxx_py_ffi, m) {
     m.def("add", &add);
 }
