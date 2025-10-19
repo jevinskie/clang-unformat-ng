231e2de8d260af521a6406f16f33f501f27b8416
Jevin Sweval <jevinsweval@gmail.com>
2025-10-19 00:20:28 -0400

    try some pseudo-types

--- python/src/clang_unformat_ng/styleopts.py
+++ python/src/clang_unformat_ng/styleopts.py
@@ -9,6 +9,13 @@ BOOL_TYPE = Type(cxx_name="bool", yaml_name="Boolean", is_list=False, is_optiona
 UINT_TYPE = Type(cxx_name="unsigned", yaml_name="Unsigned", is_list=False, is_optional=False, is_deprecated=False)
 SINT_TYPE = Type(cxx_name="int", yaml_name="Integer", is_list=False, is_optional=False, is_deprecated=False)
 STR_TYPE = Type(cxx_name="std::string", yaml_name="String", is_list=False, is_optional=False, is_deprecated=False)
+STR_LIST_TYPE = Type(
+    cxx_name="std::vector<std::string>",
+    yaml_name="List of Strings",
+    is_list=True,
+    is_optional=False,
+    is_deprecated=False,
+)
 
 
 def is_bool(ty: Type) -> bool:
@@ -29,3 +36,7 @@ def is_sint(ty: Type) -> bool:
 
 def is_str(ty: Type) -> bool:
     return ty == STR_TYPE
+
+
+def is_str_list(ty: Type) -> bool:
+    return ty == STR_LIST_TYPE
