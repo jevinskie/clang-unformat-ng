905a381c6938b21c2f3fb0a66ad6d5ee6e7806da
Jevin Sweval <jevinsweval@gmail.com>
2025-10-19 00:01:30 -0400

    py: attrs: remove field()s

--- python/src/clang_unformat_ng/styleopts_types.py
+++ python/src/clang_unformat_ng/styleopts_types.py
@@ -1,32 +1,32 @@
-from attrs import Factory, define, field
+from attrs import Factory, define
 
 
 @define(auto_attribs=True, frozen=True)
 class Type:
-    cxx_name: str = field(kw_only=True)
-    yaml_name: str = field(kw_only=True)
-    is_list: bool = field(kw_only=True)
-    is_optional: bool = field(kw_only=True)
-    is_deprecated: bool = field(kw_only=True)
+    cxx_name: str
+    yaml_name: str
+    is_list: bool
+    is_optional: bool
+    is_deprecated: bool
 
 
 @define(auto_attribs=True, frozen=True)
 class Version:
-    major: int = field(kw_only=True)
-    minor: int | None = field(default=None)
+    major: int
+    minor: int | None
 
 
 @define(auto_attribs=True, frozen=True)
 class NestedField:
-    name: str = field(kw_only=True)
-    type: Type = field(kw_only=True)
-    version: Version | None = field(default=None)
+    name: str
+    type: Type
+    version: Version | None
 
 
 @define(auto_attribs=True, frozen=True)
 class EnumValue:
-    name: str = field(kw_only=True)
-    config: str = field(kw_only=True)
+    name: str
+    config: str
 
 
 @define(auto_attribs=True, frozen=True)
@@ -40,8 +40,8 @@ class Enum:
 class NestedEnum:
     name: str
     type: Type
-    version: Version | None = field(kw_only=True)
-    values: list = field(kw_only=True)
+    version: Version | None
+    values: list
 
 
 @define(auto_attribs=True, frozen=True)
@@ -55,9 +55,9 @@ class NestedStruct:
 class Option:
     name: str
     type: Type
-    version: Version | None = field(default=None)
-    enum: Enum | None = field(default=None)
-    nested_struct: NestedStruct | None = field(default=None)
+    version: Version | None
+    enum: Enum | None
+    nested_struct: NestedStruct | None
 
 
 def enum_cxx_type_name_is_deprecated(name: str) -> bool:
