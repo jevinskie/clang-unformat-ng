3d0bd75259b1c1ace0f3d0e0b6031c746a2ee1fd
Jevin Sweval <jevinsweval@gmail.com>
2025-10-20 16:40:17 -0400

    cleanup

--- python/src/clang_unformat_ng/styleopts/types.py
+++ python/src/clang_unformat_ng/styleopts/types.py
@@ -91,16 +91,14 @@ class NestedStruct:
         return hash((self.name, self.type, tuple(sorted(enum_vals)), tuple(sorted(field_vals))))
 
 
-class OptionBase:
+@define(auto_attribs=True, frozen=True)
+class Option:
     name: str
     type: Type
     version: Version | None
     enum: Enum | None
     nested_struct: NestedStruct | None
 
-
-Option = define(auto_attribs=True, frozen=True)(OptionBase)
-MutableOption = define(auto_attribs=True)(OptionBase)
 
 # TODO: is needed? it calls attrs.resolve_types on all seralized classes
 # _resolve_json_schema_obj_cls_types()
