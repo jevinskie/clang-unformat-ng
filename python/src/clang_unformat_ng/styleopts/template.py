808c1e7e9b096c2ee964917ab65d5a9a870bab70
Jevin Sweval <jevinsweval@gmail.com>
2025-10-20 19:45:47 -0400

    wip

--- python/src/clang_unformat_ng/styleopts/template.py
+++ python/src/clang_unformat_ng/styleopts/template.py
@@ -1,4 +1,5 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
+from rich import print as rprint
 
 env = Environment(
     loader=PackageLoader("clang_unformat_ng.styleopts"),
@@ -12,3 +13,13 @@ env = Environment(
 def render_enum() -> str:
     eb = "SIAS_Never,\nSIAS_Always,\nSIAS_Leave"
     return eb
+
+
+def render_rfl() -> str:
+    r = ""
+    template = env.get_template("rfl.cpp")
+    rprint(template)
+    rendered = template.render()
+    rprint(rendered)
+    r = rendered
+    return r
--- python/src/clang_unformat_ng/tools/unformat_util.py
+++ python/src/clang_unformat_ng/tools/unformat_util.py
@@ -6,6 +6,7 @@ from rich import print
 from rich.pretty import pprint
 
 import clang_unformat_ng.styleopts.data as sopts_data
+import clang_unformat_ng.styleopts.template as sopts_tmpl
 
 
 def do_dump_opts():
@@ -15,6 +16,8 @@ def do_dump_opts():
 
 def do_template():
     print("doing template")
+    r = sopts_tmpl.render_rfl()
+    print(r)
 
 
 def do_etc():
