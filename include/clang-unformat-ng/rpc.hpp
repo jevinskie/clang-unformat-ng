6028ddb0efbde5cb0674de422d5ec26324f65b0e
Jevin Sweval <jevinsweval@gmail.com>
2025-10-20 13:52:43 -0400

    rpc command structs

--- include/clang-unformat-ng/rpc.hpp
+++ include/clang-unformat-ng/rpc.hpp
@@ -1,3 +1,40 @@
 #pragma once
 
 #include "common.hpp"
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace unformat {
+namespace rpc_cmd {
+struct HelloReq {
+    std::string hello_req;
+};
+struct HelloResp {
+    std::string hello_resp;
+};
+
+struct SetPathsReq {
+    std::vector<std::string> paths;
+};
+struct SetPathsResp {
+    bool successful;
+};
+struct SetStyleReq {
+    using style_t = std::string;
+    style_t style;
+};
+struct SetStyleResp {
+    bool succesful;
+};
+struct FormatReq {};
+struct FormatRespResult {
+    double score;
+};
+struct FormatResp {
+    bool successful;
+    std::map<std::string, FormatRespResult> results;
+};
+}; // namespace rpc_cmd
+}; // namespace unformat
