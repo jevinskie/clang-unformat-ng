03d5792349827961fa22171c5de18d46b85678f1
Jevin Sweval <jevinsweval@gmail.com>
2025-10-20 19:25:54 -0400

    pyenum

--- python/src/clang_unformat_ng/styleopts/template.py
+++ python/src/clang_unformat_ng/styleopts/template.py
@@ -1,5 +1,4 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
-from rich import print as rprint
 
 env = Environment(
     loader=PackageLoader("clang_unformat_ng.styleopts"),
@@ -10,9 +9,5 @@ env = Environment(
 )
 
 
-def _do_template_test():
-    template = env.get_template("bool.tcpp")
-    rprint(f"template: {template}")
-    rnd = template.render()
-    rprint("rnd:")
-    rprint(rnd)
+def render_enum() -> str:
+    pass
---
+++ python/tests/test_style_templates.py
@@ -0,0 +1,47 @@
+from clang_unformat_ng.styleopts.template import render_enum
+from clang_unformat_ng.styleopts.types import Enum, EnumValue, Option, Type, Version
+from clang_unformat_ng.utils import shrink_spaces as sp
+
+
+def test_styp():
+    opt = Option(
+        name="SpacesInAngles",
+        type=Type(
+            cxx_name="SpacesInAnglesStyle",
+            yaml_name="SpacesInAnglesStyle",
+            is_list=False,
+            is_optional=False,
+            is_deprecated=False,
+        ),
+        version=Version(major=3, minor=4),
+        enum=Enum(
+            name="SpacesInAnglesStyle",
+            type=Type(
+                cxx_name="SpacesInAnglesStyle",
+                yaml_name="SpacesInAnglesStyle",
+                is_list=False,
+                is_optional=False,
+                is_deprecated=False,
+            ),
+            values=[
+                EnumValue(name="SIAS_Never", config="SIAS_Never"),
+                EnumValue(name="SIAS_Always", config="SIAS_Always"),
+                EnumValue(name="SIAS_Leave", config="SIAS_Leave"),
+            ],
+        ),
+        nested_struct=None,
+    )
+    golden = """
+enum SpacesInAnglesStyle {
+  SIAS_Never,
+  SIAS_Always,
+  SIAS_Leave
+};
+SpacesInAnglesStyle SpacesInAngles;
+"""
+    rendered = render_enum(opt)
+    print("golden:")
+    print(golden)
+    print("rendered:")
+    print(rendered)
+    assert sp(golden) == sp(rendered)
