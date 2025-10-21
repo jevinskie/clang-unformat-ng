16b0868ab8914099d55e0a585cfdc5d182d823f0
Jevin Sweval <jevinsweval@gmail.com>
2025-10-20 20:56:04 -0400

    wip

--- python/src/clang_unformat_ng/styleopts/template.py
+++ python/src/clang_unformat_ng/styleopts/template.py
@@ -1,7 +1,7 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 # from rich import print as rprint
-from clang_unformat_ng.styleopts.types import Option
+from clang_unformat_ng.styleopts.types import UNION_TYPE_TUPLE, Option
 
 env = Environment(
     loader=PackageLoader("clang_unformat_ng.styleopts"),
@@ -20,6 +20,7 @@ def render_enum(opt: Option) -> str:
 def render_rfl(opts: list[Option], **kwargs) -> str:
     template = env.get_template("rfl.tcpp")
     enums = filter(lambda x: x.enum is not None, opts)
+    other = filter(lambda x: x.type in UNION_TYPE_TUPLE, opts)
     # other = filter(lambda x: x.enum is None and x.nested_struct is None, opts)
     # rprint(list(other))
-    return template.render(enums=enums)
+    return template.render(enums=enums, other=other)
