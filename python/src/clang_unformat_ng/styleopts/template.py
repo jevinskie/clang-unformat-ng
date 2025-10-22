dc8b25682130efe8d260a7b34d54bbdd23941d7c
Jevin Sweval <jevinsweval@gmail.com>
2025-10-21 21:05:05 -0400

    bump

--- python/src/clang_unformat_ng/styleopts/template.py
+++ python/src/clang_unformat_ng/styleopts/template.py
@@ -3,7 +3,7 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 from rich import print as rprint
 
 # from rich import print as rprint
-from clang_unformat_ng.styleopts.types import BOOL_TYPE, SINT_TYPE, UINT_TYPE, Option
+from clang_unformat_ng.styleopts.types import BOOL_TYPE, SINT_TYPE, UINT_TYPE, UNION_STD_TYPE_TUPLE, Option
 
 env = Environment(
     loader=PackageLoader("clang_unformat_ng.styleopts"),
@@ -22,23 +22,24 @@ def render_enum(opt: Option) -> str:
 def render_rfl(opts: list[Option], **kwargs) -> str:
     template = env.get_template("rfl.tcpp")
     enums = list(filter(lambda x: x.enum is not None, opts))
+    std = filter(lambda x: x.type in UNION_STD_TYPE_TUPLE, opts)
     # rprint(enums)
-    # std = filter(lambda x: x.type in UNION_STD_TYPE_TUPLE, opts)
+    std = filter(lambda x: x.type in UNION_STD_TYPE_TUPLE, opts)
     # enums = tuple()
     filter(lambda x: x.type in (BOOL_TYPE, UINT_TYPE, SINT_TYPE), opts)
 
-    enums_none = list(filter(lambda x: x.type.cxx_qual is not None, enums))
-    enums_some = list(
-        filter(
-            lambda x: evolve(x, **{"type": evolve(x.type, **{"cxx_qual": "clang::format::FormatStyle"})}), enums_none
-        )
+    enums_none = list(filter(lambda x: x.type.cxx_qual is None, enums))
+    enums_new = list(
+        map(lambda x: evolve(x, **{"type": evolve(x.type, **{"cxx_qual": "clang::format::FormatStyle"})}), enums_none)
     )
-
-    list(filter(lambda x: x.type.cxx_qual is not None, enums))
+    rprint(enums_new)
 
     # other = filter(lambda x: x.enum is None and x.nested_struct is None, opts)
     # rprint(list(other))
     # lopts = list((*std, *enums))
-    lopts = list((*enums_some,))
-    rprint(lopts)
+    lopts = list((
+        *std,
+        *enums_new,
+    ))
+    # rprint(lopts)
     return template.render(opts=lopts)
