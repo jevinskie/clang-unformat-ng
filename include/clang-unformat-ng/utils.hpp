baff0a6bc289d8604af77e3754d78e88a9fbb84f
Jevin Sweval <jevinsweval@gmail.com>
2025-10-23 21:32:02 -0400

    wip

--- include/clang-unformat-ng/utils.hpp
+++ include/clang-unformat-ng/utils.hpp
@@ -19,26 +19,28 @@ public:
     UnixSocket(const std::string &path);
     ~UnixSocket();
 
+    std::vector<uint8_t> read_raw(size_t size);
+    void read_raw(std::span<uint8_t> buf);
     std::vector<uint8_t> read(size_t size);
     void read(std::span<uint8_t> buf);
     template <typename T> T read() {
         constexpr auto sz = sizeof(T);
         static_assert(sz <= std::numeric_limits<T>::max(), "msg sz is >= UINT32_MAX");
         uint32_t sz_u32{};
-        read({reinterpret_cast<uint8_t *>(&sz_u32), sizeof(uint32_t)});
+        read_raw({reinterpret_cast<uint8_t *>(&sz_u32), sizeof(uint32_t)});
         T res{};
-        read({reinterpret_cast<uint8_t *>(&res), sizeof(T)});
-
+        read_raw({reinterpret_cast<uint8_t *>(&res), sizeof(T)});
         return res;
     }
 
+    void write_raw(std::span<const uint8_t> buf);
     void write(std::span<const uint8_t> buf);
     template <typename T> void write(const T &value) {
         constexpr auto sz = sizeof(T);
         static_assert(sz <= std::numeric_limits<T>::max(), "msg sz is >= UINT32_MAX");
         const uint32_t sz_u32{sz};
-        write({static_cast<uint8_t *>(std::remove_cv_t<uint32_t>(&sz_u32)), sizeof(uint32_t)});
-        write({static_cast<uint8_t *>(&value), sz});
+        write_raw({static_cast<uint8_t *>(std::remove_cv_t<uint32_t>(&sz_u32)), sizeof(uint32_t)});
+        write_raw({static_cast<uint8_t *>(&value), sz});
     }
 
     void shutdown();
